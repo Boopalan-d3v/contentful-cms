cf6d4a1a5107053c0259ab4f7f5c3bfcd57e8c5a
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-20 11:36:03 +0530

    done Hero component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import heroImg from './assets/hero.svg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="hero">
+        <div className="hero-center">
+            <div className="hero-title">
+                <h1>Contentful CMS</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum tempora fugit neque minus. Unde harum eligendi ea quis ab nisi amet, quae, laboriosam nemo error modi architecto repellat impedit illo.</p>
+            </div>
+            <div className="img-container">
+                <img src={heroImg} alt='Women and the browser' className='img'></img>
+            </div>
+        </div>
+    </section>
   )
 }
 
