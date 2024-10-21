89faa4c88726cd0e8351d5f07a39068adc0d12d5
MyCoding-Hub <breacher.darkworld@gmail.com>
2024-10-21 13:22:44 +0530

    done functionality for projects component

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useFetchProjects } from "./fetchProjects"
 
 const Projects = () => {
+  const {isLoading, projects } = useFetchProjects()
+  console.log(isLoading,projects);
+
+  if (isLoading) {
+    return <section className="projects">
+      <h2>Loading...</h2>
+    </section>
+  }
+  
   return (
-    <div>Projects</div>
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => {
+          const {title, url, id, img} = project
+          return <a key= {id} href={url} target="_blank" rel="noreferrer" className="project">
+            <img src={img} alt={title} className="img"></img>
+            <h5>{title}</h5>
+          </a>
+        })}
+      </div>
+    </section>
   )
 }
 
